558f51ab06b1082b0b40cd4f8996b5c806815882
Arthur Guerra <arthur_gb1996@hotmail.com>
2021-12-04 17:23:52 -0300

    altera exercício 4

--- src/m1/s5/Ex4.java
+++ src/m1/s5/Ex4.java
@@ -11,11 +11,19 @@ public class Ex4 {
     // seu código aqui
     }*/
 
-    private final int _FINAL_ATTRIBUTE1_ = 0;
-    private final static int _FINAL_ATTRIBUTE2_ = 1;
+    private final int _FINAL_ATTRIBUTE1_;
+    private final static int _FINAL_ATTRIBUTE2_;
+
+    static {
+        _FINAL_ATTRIBUTE2_ = 1;
+    }
+
+    Ex4() {
+        _FINAL_ATTRIBUTE1_ = 0;
+    }
 
     public static void main(String[] args) {
         System.out.println(new Ex4()._FINAL_ATTRIBUTE1_);
-        System.out.println(_FINAL_ATTRIBUTE2_);
+        System.out.println(Ex4._FINAL_ATTRIBUTE2_);
     }
 }
